34a9c53b62048a7e42089914ea70d377222def99
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2022-06-12 09:50:07 +0530

    Add files via upload

---
+++ Complete full code/complete/LCD.h
@@ -0,0 +1,174 @@
+/*
+ * _1602LCDI2C.h
+ *
+ * Created: 5/16/2020 12:32:15 AM
+ *  Author: Dilan
+ */ 
+
+
+#ifndef LCDI2C_H_
+#define LCDI2C_H_
+
+#define LCD_CLEARDISPLAY 0x01
+#define LCD_RETURNHOME 0x02
+#define LCD_ENTRYMODESET 0x04
+#define LCD_DISPLAYCONTROL 0x08
+#define LCD_CURSORSHIFT 0x10
+#define LCD_FUNCTIONSET 0x20
+#define LCD_SETCGRAMADDR 0x40
+#define LCD_SETDDRAMADDR 0x80
+
+// flags for display entry mode
+#define LCD_ENTRYRIGHT 0x00
+#define LCD_ENTRYLEFT 0x02
+#define LCD_ENTRYSHIFTINCREMENT 0x01
+#define LCD_ENTRYSHIFTDECREMENT 0x00
+
+// flags for display on/off control
+#define LCD_DISPLAYON 0x04
+#define LCD_DISPLAYOFF 0x00
+#define LCD_CURSORON 0x02
+#define LCD_CURSOROFF 0x00
+#define LCD_BLINKON 0x01
+#define LCD_BLINKOFF 0x00
+
+// flags for display/cursor shift
+#define LCD_DISPLAYMOVE 0x08
+#define LCD_CURSORMOVE 0x00
+#define LCD_MOVERIGHT 0x04
+#define LCD_MOVELEFT 0x00
+
+// flags for function set
+#define LCD_8BITMODE 0x10
+#define LCD_4BITMODE 0x00
+#define LCD_2LINE 0x08
+#define LCD_1LINE 0x00
+#define LCD_5x10DOTS 0x04
+#define LCD_5x8DOTS 0x00
+
+
+//LcdCommand(LCD_DISPLAYCONTROL|LCD_DISPLAYON|LCD_BLINKON);  Blinlk on
+//LcdCommand(LCD_CURSORSHIFT|LCD_CURSORMOVE|LCD_MOVERIGHT);  Cursor right
+//LcdCommand(LCD_CURSORSHIFT|LCD_DISPLAYMOVE|LCD_MOVELEFT);  Display shift
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+
+void LcdInit();
+void LcdCommand(uint8_t command);
+void Lcd4bitdata(uint8_t bitdata,uint8_t RSvalue);
+void LcdChar(uint8_t Lcdcharval);
+void LcdString (char *str);
+void LcdBacklight(uint8_t Backlightdata);
+void LcdSetCursor(uint8_t xaxis, uint8_t yaxis,char*str);
+void LcdCursor(uint8_t Cursor);
+void LcdScroll(uint8_t side);
+
+
+
+uint8_t deviceAdress; //I2C Adress
+uint8_t Backlight=1; //Backlight state
+
+
+/*Define Enable & Reset */
+#define EN 0b00000100
+#define RST 0b00000001
+
+
+
+
+
+void LcdInit(){
+	LcdBacklight(1);
+	_delay_ms(20); // LCD power On delay
+	
+	DDRD=0xFF;
+	
+	
+	LcdCommand(LCD_RETURNHOME);		/*Return home  */
+
+	LcdCommand(LCD_FUNCTIONSET|LCD_4BITMODE|LCD_2LINE);              /* 2 line, 5*7 matrix in 4-bit mode */
+	LcdCommand(LCD_DISPLAYCONTROL|LCD_DISPLAYON|LCD_CURSOROFF);              /* Display on cursor off*/
+	LcdCommand(LCD_ENTRYMODESET|LCD_ENTRYLEFT|LCD_ENTRYSHIFTDECREMENT);              /* Increment cursor (shift cursor to right)*/
+	LcdCommand(LCD_CLEARDISPLAY);
+	
+	
+	
+}
+
+void LcdBacklight(uint8_t Backlightdata){
+	if (Backlightdata==1)
+	{Backlight=0b00001000;
+	} 
+	else
+	{Backlight=0b00000000;
+	}
+	
+}
+
+void LcdCommand(uint8_t command){
+	Lcd4bitdata(command&0xF0,0);
+	Lcd4bitdata((command<<4)&0xF0,0);
+	
+}
+
+void LcdChar(uint8_t Lcdcharval){
+	Lcd4bitdata(Lcdcharval&0xF0,1);
+	Lcd4bitdata((Lcdcharval<<4)&0xF0,1);
+	
+}
+
+void LcdSetCursor(uint8_t xaxis, uint8_t yaxis,char*str){
+	uint8_t tempcommand;
+	switch(yaxis){
+		case 0:tempcommand=0x80;break;  //yaxis =0
+		case 1:tempcommand=0xC0;break;
+		case 2:tempcommand=0x94;break;
+		case 3:tempcommand=0xd4;break;		
+	}
+	LcdCommand(tempcommand|(0x0F&xaxis));
+	LcdString(str);
+	
+}
+
+void LcdString (char *str)		/* Send string to LCD function */
+{
+	int i;
+	for(i=0;str[i]!=0;i++)		/* Send each char of string till the NULL */
+	{
+		LcdChar(str[i]);
+	}
+}
+void LcdCursor(uint8_t Cursor){
+	if (Cursor==0)
+	{LcdCommand(0xC);
+	} 
+	else
+	{
+	}
+	
+}
+
+
+
+void Lcd4bitdata(uint8_t bitdata,uint8_t RSvalue){
+	
+	PORTD=((bitdata|RSvalue|Backlight));
+	
+	PORTD=(((bitdata|EN)|RSvalue|Backlight));
+	
+	_delay_us(200);
+	
+	PORTD=(((bitdata&~EN)|RSvalue|Backlight));
+	
+	_delay_ms(2);
+	
+}
+
+
+
+
+#endif /* 1602LCDI2C_H_ */
+
---
+++ Complete full code/complete/main.c
@@ -0,0 +1,396 @@
+#define F_CPU 8000000
+#include <avr/io.h>
+#include <stdio.h>
+#include "LCD.h"
+
+const uint8_t kaypad[4][4]={{7,8,9,10},{4,5,6,11},{1,2,3,12},{13,0,14,15}};
+uint8_t getkeyNum();
+char lcddata[20];
+uint8_t key;
+
+uint16_t MOIS_Reading=0; //analog Reading variable
+uint16_t TempReading=0; //analog Reading variable
+uint16_t ReadADC(uint8_t ADCchannel); //analog reading function
+
+uint16_t templimit=0;
+void getTemp();
+uint16_t moisturelimit=0;
+void getMoist();
+uint16_t weightlimit=0;
+void getweight();
+
+#define ControlPort PORTC
+#define ControlDDR DDRC
+#define ControlPIN PINC
+#define DIRL 4
+#define STEPL 3
+#define STEP1 2
+#define gearMotor 5
+#define blower 6
+#define heater 7
+
+void stepperE(uint8_t side,uint16_t cycle);////////////////////////////
+void stepperMotor(uint8_t side,uint16_t cycle);
+void stepperR(uint8_t side,uint16_t cycle);
+uint8_t hx711H=0; //Load Scale High Bits
+uint16_t hx711L=0;//Load Scale Low Bits
+float loadCellRead();
+
+int main(void)
+{
+   LcdInit();
+   ControlDDR|=(1<<DIRL)|(1<<STEPL)|(1<<STEP1)|(1<<gearMotor)|(1<<blower)|(1<<heater)|(1<<1); 
+   PORTC&=~(1<<0);//Clock pin low
+   ADCSRA |= ((1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0));   // prescaler 128
+   ADMUX |= (1<<REFS0);					//AVCC
+   ADCSRA |= (1<<ADEN);                 // Turn on ADC
+   while(1){
+	   key=getkeyNum();
+	   if (key==13)
+	   {break;
+	   }
+	   
+   }
+   LcdSetCursor(4,0,"Welcome");
+   _delay_ms(500);
+   LcdCommand(LCD_CLEARDISPLAY);
+   
+   getTemp();
+   getMoist();
+   LcdCommand(LCD_CLEARDISPLAY);
+   LcdSetCursor(0,0,"Starting");
+   
+   ControlPort|=(1<<gearMotor);
+   _delay_ms(1000);
+    ControlPort&=~(1<<gearMotor);
+	
+  stepperMotor(1,500);
+  ControlPort|=(1<<blower)|(1<<heater); 
+    while (1) 
+    {
+		MOIS_Reading=ReadADC(1); //calibrated number
+		TempReading=(ReadADC(0)*0.50048876); //calibrated number
+		
+		
+		sprintf(lcddata,"Moist. %04u",MOIS_Reading);
+		LcdSetCursor(0,0,lcddata);
+		sprintf(lcddata,"Temp %03uC",TempReading);
+		LcdSetCursor(0,1,lcddata);
+		
+		if ((MOIS_Reading<moisturelimit)&&(TempReading>templimit))
+		{LcdCommand(LCD_CLEARDISPLAY);
+			LcdSetCursor(0,0,"Limits Reached");
+			_delay_ms(2000);//////////////////////////////////////clean up delay
+			ControlPort&=~((1<<gearMotor)|(1<<blower)|(1<<heater)); 
+			break;
+		}
+		
+    }
+	
+	stepperR(1,500);
+	
+	 ControlPort|=(1<<gearMotor);
+	 _delay_ms(1000);
+	 ControlPort&=~(1<<gearMotor);
+	 stepperMotor(0,1000);
+	 //stepperR(0,1000);
+	 	uint8_t breakv=0;
+	/*A start*/
+	while(1){
+		breakv=0;
+		getweight();
+		sprintf(lcddata,"limit   %1.3f kg",weightlimit);
+		LcdSetCursor(0,0,lcddata);
+		while(1){
+			float readW=loadCellRead();
+			sprintf(lcddata,"Reading %1.3f kg",readW);
+			LcdSetCursor(0,1,lcddata);
+			
+			for(uint8_t i=0;i<100;i++){
+				_delay_us(1000);
+				stepperE(0,1000);             ///////////////////////////////
+				if (readW>=weightlimit)
+				{LcdCommand(LCD_CLEARDISPLAY);
+					LcdSetCursor(0,0,"Process done!");
+					stepperE(0,500);
+					_delay_ms(1000);
+					breakv=1;
+					break;
+				}
+			}
+			
+			if (breakv)
+			{break;
+			}
+			
+		
+	}
+	
+	/*A end*/
+	
+
+	}
+	
+}
+
+uint8_t getkeyNum(){
+	DDRB=0b00001111;
+	PORTB=0b11110000;
+	uint8_t getx;
+	uint8_t gety;
+	while(1){
+		
+		uint8_t pin=(~(PINB|0x0F));
+		if (pin)
+		{
+			
+			
+			switch(pin){
+				
+				
+				case (1<<4) :getx=0; break;
+				case (1<<5) :getx=1;break;
+				case (1<<6) :getx=2;break;
+				case (1<<7) :getx=3;break;
+				
+				
+			} //switch
+			DDRB=0b0;
+			PORTB=0b0;
+			
+			DDRB=0b11110000;
+			PORTB=0b00001111;
+			
+			pin=~(PINB|0xF0);
+			
+			switch(pin){
+				case (1<<0) :gety=0;break;
+				case (1<<1) :gety=1;break;
+				case (1<<2) :gety=2;break;
+				case (1<<3) :gety=3;break;
+				
+			} //switch
+			
+			DDRB=0b00001111;
+			PORTB=0b11110000;
+			
+			
+			return kaypad[gety][getx];
+			
+		}//if
+		
+		
+		
+	}//while 1
+}
+
+void getTemp(){
+	
+	LcdCommand(LCD_CLEARDISPLAY);
+	LcdSetCursor(0,0,"Set temperature");
+	_delay_ms(500);
+	uint8_t pos=0;
+	uint8_t numbers[3]={0,0,0};
+	sprintf(lcddata,"%u%u%u",numbers[0],numbers[1],numbers[2]);
+	LcdSetCursor(0,1,lcddata);
+	LcdSetCursor(pos,1,"");
+	LcdCommand(LCD_DISPLAYCONTROL|LCD_DISPLAYON|LCD_BLINKON);
+	
+	while(1){
+		key=getkeyNum();
+		if (key<10)
+		{
+			numbers[pos]=key;
+			sprintf(lcddata,"%u%u%u",numbers[0],numbers[1],numbers[2]);
+			LcdSetCursor(0,1,lcddata);
+			pos++;
+			if (pos>2)
+			{pos=0;
+			}
+			LcdSetCursor(pos,1,"");
+			_delay_ms(200);
+		}
+		
+		else if (key==14)
+		{LcdCommand(LCD_CLEARDISPLAY);
+		LcdCommand(LCD_DISPLAYCONTROL|LCD_DISPLAYON|LCD_BLINKOFF);
+		break;
+		}
+	}
+	templimit=(numbers[0]*100)+(numbers[1]*10)+numbers[2];
+	LcdSetCursor(0,0,"Temperature set!");
+	sprintf(lcddata,"%u",templimit);
+	LcdSetCursor(0,1,lcddata);
+	_delay_ms(1000);
+	LcdCommand(LCD_CLEARDISPLAY);
+	
+}
+
+void getMoist(){
+	LcdCommand(LCD_CLEARDISPLAY);
+	LcdSetCursor(0,0,"Set Moisture");
+	_delay_ms(500);
+	uint8_t pos=0;
+	uint8_t numbers[4]={0,0,0,0};
+	sprintf(lcddata,"%u%u%u%u",numbers[0],numbers[1],numbers[2],numbers[3]);
+	LcdSetCursor(0,1,lcddata);
+	LcdSetCursor(pos,1,"");
+	LcdCommand(LCD_DISPLAYCONTROL|LCD_DISPLAYON|LCD_BLINKON);
+	
+	while(1){
+		key=getkeyNum();
+		if (key<10)
+		{
+			numbers[pos]=key;
+			sprintf(lcddata,"%u%u%u%u",numbers[0],numbers[1],numbers[2],numbers[3]);
+			LcdSetCursor(0,1,lcddata);
+			pos++;
+			if (pos>3)
+			{pos=0;
+			}
+			LcdSetCursor(pos,1,"");
+			_delay_ms(200);
+		}
+		
+		else if (key==14)
+		{LcdCommand(LCD_CLEARDISPLAY);
+			LcdCommand(LCD_DISPLAYCONTROL|LCD_DISPLAYON|LCD_BLINKOFF);
+			break;
+		}
+	}
+	moisturelimit=(numbers[0]*1000)+(numbers[1]*100)+(numbers[2]*10)+numbers[3];
+	LcdSetCursor(0,0,"Moisture set!");
+	sprintf(lcddata,"%u",moisturelimit);
+	LcdSetCursor(0,1,lcddata);
+	_delay_ms(1000);
+	LcdCommand(LCD_CLEARDISPLAY);	
+	
+	
+}
+
+void getweight(){
+	LcdCommand(LCD_CLEARDISPLAY);
+	LcdSetCursor(0,0,"Set Weight(g)");
+	_delay_ms(500);
+	uint8_t pos=0;
+	uint8_t numbers[4]={0,0,0,0};
+	sprintf(lcddata,"%u%u%u%u",numbers[0],numbers[1],numbers[2],numbers[3]);
+	LcdSetCursor(0,1,lcddata);
+	LcdSetCursor(pos,1,"");
+	LcdCommand(LCD_DISPLAYCONTROL|LCD_DISPLAYON|LCD_BLINKON);
+	
+	while(1){
+		key=getkeyNum();
+		if (key<10)
+		{
+			numbers[pos]=key;
+			sprintf(lcddata,"%u%u%u%u",numbers[0],numbers[1],numbers[2],numbers[3]);
+			LcdSetCursor(0,1,lcddata);
+			pos++;
+			if (pos>3)
+			{pos=0;
+			}
+			LcdSetCursor(pos,1,"");
+			_delay_ms(200);
+		}
+		
+		else if (key==14)
+		{LcdCommand(LCD_CLEARDISPLAY);
+			LcdCommand(LCD_DISPLAYCONTROL|LCD_DISPLAYON|LCD_BLINKOFF);
+			break;
+		}
+	}
+	weightlimit=(numbers[0]*1000)+(numbers[1]*100)+(numbers[2]*10)+numbers[3];
+	LcdSetCursor(0,0,"Weight set!");
+	sprintf(lcddata,"%ug",weightlimit);
+	LcdSetCursor(0,1,lcddata);
+	_delay_ms(500);
+	LcdCommand(LCD_CLEARDISPLAY);	
+	
+}
+
+void stepperMotor(uint8_t side,uint16_t cycle){
+	ControlPort=(ControlPort&~(1<<DIRL))|(side<<DIRL);
+
+	for(uint16_t i=0;i<cycle;i++){
+		ControlPort|=(1<<STEPL);
+		_delay_us(1000);
+		ControlPort&=~(1<<STEPL);
+		_delay_us(1000);
+	}
+}
+
+void stepperR(uint8_t side,uint16_t cycle){
+	ControlPort=(ControlPort&~(1<<DIRL))|(side<<DIRL);
+
+	for(uint16_t i=0;i<cycle;i++){
+		ControlPort|=(1<<STEP1);
+		_delay_us(1000);
+		ControlPort&=~(1<<STEP1);
+		_delay_us(1000);
+	}
+}
+
+void stepperE(uint8_t side,uint16_t cycle){
+	ControlPort=(ControlPort&~(1<<DIRL))|(side<<DIRL);
+
+	for(uint16_t i=0;i<cycle;i++){
+		PORTD|=(1<<3);
+		_delay_us(1000);
+		PORTD&=~(1<<3);
+		_delay_us(1000);
+	}
+}
+
+uint16_t ReadADC(uint8_t ADCchannel)
+{
+	//select ADC channel with safety mask
+	ADMUX = (ADMUX & 0xF0) | (ADCchannel & 0x0F);
+	//single conversion mode
+	ADCSRA |= (1<<ADSC);
+	// wait until ADC conversion is complete
+	while( ADCSRA & (1<<ADSC) );
+	return ADCW;
+}
+
+float loadCellRead(){
+	hx711H=0;hx711L=0;  //clear variables
+	for(uint8_t i=0;i<8;i++){  // Load cell data high 8 bits
+		PORTC|=(1<<0); //Clock pin high
+		_delay_us(10);
+		if ((PINC&(1<<1))>>1)  //read data pin
+		{hx711H|=(1<<(7-i));//set hx 711 varible
+		}
+		else
+		{hx711H&=~(1<<(7-i));
+		}
+		PORTC&=~(1<<0); //Clock pin low
+		_delay_us(5);
+	}
+	
+	
+	for(uint8_t i=0;i<16;i++){ // Load cell data low 16 bits
+		PORTC|=(1<<0); //Clock pin high
+		_delay_us(10);
+		if ((PINC&(1<<1))>>1) //read data pin
+		{hx711L|=(1<<(15-i));
+		}
+		else
+		{hx711L&=~(1<<(15-i));
+		}
+		PORTC&=~(1<<0); //Clock pin low
+		_delay_us(5);
+	}
+	
+	hx711L=hx711L>>1; //shift bits
+	
+	if (hx711H&1)  //bit setup
+	{hx711L|=(1<<15);
+	}
+	else
+	{hx711L&=~(1<<15);
+	}
+	hx711H=hx711H>>1;
+	
+	return (hx711H*(65536/18029.6))+hx711L/18029.6; //load cell calibration
+}
